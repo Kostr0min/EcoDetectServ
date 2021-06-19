c1ee626cc8b9c53709aa64ca4f824c1868eebcec
Kostr0min <dskostromin@gmail.com>
2021-06-19 11:49:23 +0300

    init

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image_dataset_from_directory
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
 import uvicorn
 from pydantic import BaseModel
 from fastapi import FastAPI
 from typing import List
+
 import numpy as np
+import torch.nn as nn
+from PIL import Image
+
+import torchvision
+from torchvision import datasets, models, transforms
+
+import torch
 
 app = FastAPI()
 
-model = tf.keras.models.load_model('models/22_04_2021.h5')
+model = torchvision.models.mobilenet_v2(pretrained=True, progress=True)
+
+model_ft = model
+model_ft.fc = nn.Linear(1280, 6)
+
+model_ft.load_state_dict(torch.load('./models/model_pytorch'))
+model_ft.eval()
 
 
 class Images(BaseModel):
     name: str
     img: List[int] = None
+    shape: tuple
 
 
 @app.get('/')
@@ -31,24 +42,39 @@ def predict_label(data: Images):
     Returns:
         prediction: image label
     """
-    res = {'battery': 0,
-     'cans': 1,
-     'glassbottle': 2,
-     'medicinebottle': 3,
-     'milkbox': 4,
-     'pesticidebottle': 5,
-     'plastic_cont_mark1': 6,
-     'plasticbottle': 7,
-     'toothbrush': 8,
-     'toothpastetube': 9}
-    res = list(res.keys())
+    class_names = ['cans',
+     'glassbottle',
+     'medicinebottle',
+     'pesticidebottle',
+     'plastic_cont_mark1',
+     'plasticbottle']
+
+    data_transforms = {
+        'test': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
     data = data.dict()
     img = data['img']
-    img = np.array(img, dtype='uint8').reshape(1, 128, 128, 3)
-    img_f = tf.cast(img, tf.float32)
-    prediction = model.predict(img_f)
+    shape = data['shape']
+    img = np.array(img, dtype='uint8').reshape(shape[0], shape[1], 3)
+    input_image = Image.fromarray(img)
+    input_tensor = data_transforms['test'](input_image)
+    input_batch = input_tensor.unsqueeze(0)
+    if torch.cuda.is_available():
+        input_batch = input_batch.to('cuda')
+        model_ft.to('cuda')
+    with torch.no_grad():
+        output = model_ft(input_batch)
+        _, preds = torch.max(output, 1)
+        print(preds)
+
     return {
-        'prediction': res[prediction.argmax()]
+        'prediction': class_names[preds]
     }
 
 
